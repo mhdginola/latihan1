7e1e62b82ebb69374e12832013427dc6eaa8d403
mhdginola <mhdginola@gmail.com>
2022-05-24 23:21:02 +0700

    mana

---
+++ pages/product.tsx
@@ -0,0 +1,11 @@
+import EyeN3 from '../masok/Component/EyeN3/EyeN3';
+import Layout1 from '../masok/Layout/Layout1/Layout1';
+import ContactUsForm from '../masok/Container/ContactUsForm/ContactUsForm';
+
+export default function Product(){
+  return (
+    <Layout1>
+      <EyeN3/>
+    </Layout1>
+  )
+}
