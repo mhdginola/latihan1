82cc207945256500ba92a71f7b5edcfbf4dd6336
mhdginola <mhdginola@gmail.com>
2022-05-24 23:44:31 +0700

    ganti posisi

--- masok/Component/EyeN3/EyeN3.tsx
+++ masok/Component/EyeN3/EyeN3.tsx
@@ -9,15 +9,15 @@ const Spline = dynamic(() => import('@splinetool/react-spline'), {
 export default function EyeN3() {
   return (    
     <div className={st.eyee}>
-        <div className={st.uta}>        
-            <Spline scene="https://prod.spline.design/66r8CG8Gmx61L5E7/scene.splinecode"/>
-        </div> 
         <div className={st.tekb}>
             <Image src="/images/bgb2.jpg" layout='fill' alt="back" priority/>     
         </div>
         <div className={st.te}>
           <p>a smarter way of living</p>        
-        </div>    
+        </div>  
+        <div className={st.uta}>        
+            <Spline scene="https://prod.spline.design/66r8CG8Gmx61L5E7/scene.splinecode"/>
+        </div>   
     </div>
     
   );
